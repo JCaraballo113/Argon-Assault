f5dabf14bffe317ce3b473fd3afdd4cccd1c3347
John Caraballo <jcaraballo019@gmail.com>
2019-02-15 16:31:03 -0400

    Ship movement

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour
+{
+    [Range(1,10)][SerializeField] float musicDelay = 3f;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time > musicDelay)
+        {
+            LoadNextScene();
+            this.GetComponent<MusicManager>().enabled = false;
+        }
+    }
+
+    private static void LoadNextScene()
+    {
+        SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex + 1) % SceneManager.sceneCountInBuildSettings);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float speed = 4f;
+    [SerializeField] float xRange = 5f;
+    [SerializeField] float yRange = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(Camera.main.ScreenToWorldPoint(new Vector3(1, 0, 0)));
+        HandleInput();
+    }
+
+    private void HandleInput()
+    {
+        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float verticalThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+        if(horizontalThrow > 0 || horizontalThrow < 0)
+        {
+            float xOffset = speed * Time.deltaTime * horizontalThrow;
+            float rawNewXPos = transform.localPosition.x + xOffset;
+            float clamedXPos = Mathf.Clamp(rawNewXPos, -xRange, xRange);
+            Vector3 moveVector = new Vector3(clamedXPos, transform.localPosition.y, transform.localPosition.z);
+            transform.localPosition = moveVector;
+        }
+
+        if(verticalThrow > 0 || verticalThrow < 0)
+        {
+            float yOffset = speed * Time.deltaTime * verticalThrow;
+            float rawYPos = transform.localPosition.y + yOffset;
+            float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+            Vector3 moveVector = new Vector3(transform.localPosition.x, clampedYPos, transform.localPosition.z);
+
+            transform.localPosition = moveVector;
+        }
+    }
+}
